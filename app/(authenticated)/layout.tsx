3f057923aec636ac677b4a69a26383a7dec867a3
djefarias <djefarias@gmail.com>
2026-05-06 03:17:41 -0300

    fix: remove userEmail do layout

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,5 +1,5 @@
 import AppShellWrapper from '@/components/app-shell-wrapper';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return <AppShellWrapper userName="Jefferson" userEmail="dje.reis.17@gmail.com">{children}</AppShellWrapper>;
+  return <AppShellWrapper userName="Jefferson">{children}</AppShellWrapper>;
 }
